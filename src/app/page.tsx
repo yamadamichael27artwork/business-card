4c32490935a37947cbf2671267189de71258e624
yamada_co_jp <f.atsuya.personal@gmail.com>
2024-11-01 02:00:20 +0900

    five

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,23 +3,27 @@ import Image from 'next/image'
 
 const Home = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="bg-white shadow-md rounded-lg p-8 max-w-xs text-center">
+    <div className="flex items-center justify-center min-h-screen bg-gray-100 px-4">
+      <div className="bg-gray-200 rounded-xl shadow-neumorphism p-8 max-w-2xl w-full md:max-w-3xl text-center md:text-left flex flex-col md:flex-row items-center md:items-start">
         <Image
           src="/business-card/1.png"
           alt="Profile Picture"
-          width={100}
-          height={100}
-          className="rounded-full mx-auto mb-4"
+          width={150}
+          height={150}
+          className="rounded-full mb-4 md:mb-0 md:mr-8 shadow-neumorphism"
           unoptimized
         />
-        <h1 className="text-2xl font-semibold text-gray-800">山田 太郎</h1>
-        <p className="text-gray-500">フロントエンドエンジニア</p>
-        <p className="mt-2 text-gray-600">yamada@example.com</p>
-        <p className="text-gray-600">+81-90-1234-5678</p>
-        <div className="flex justify-center mt-4 space-x-4">
-          <a href="https://twitter.com/yourhandle" target="_blank" className="text-blue-500 hover:text-blue-600">Twitter</a>
-          <a href="https://linkedin.com/in/yourhandle" target="_blank" className="text-blue-500 hover:text-blue-600">LinkedIn</a>
+        <div>
+          <h1 className="text-3xl font-semibold text-gray-800">古田　篤哉</h1>
+          <p className="text-gray-500">27卒IT学生</p>
+          <p className="mt-2 text-gray-600">f.atsuya.personal@gmail.com</p>
+          <div className="flex justify-center md:justify-start mt-4 space-x-4">
+            <a href="https://x.com/yamadacojp27" target="_blank" className="text-blue-500 hover:text-blue-600">Twitter</a>
+            <a href="https://github.com/yamadacojp27" target="_blank" className="text-blue-500 hover:text-blue-600">GitHub</a>
+            <a href="https://qiita.com/yamadacojp27" target="_blank" className="text-blue-500 hover:text-blue-600">Qiita</a>
+            <a href="https://connpass.com/user/yamada_co_jp/" target="_blank" className="text-blue-500 hover:text-blue-600">connpass</a>
+            <a href="https://www.wantedly.com/id/atsuya2027" target="_blank" className="text-blue-500 hover:text-blue-600">wantedly</a>
+          </div>
         </div>
       </div>
     </div>
