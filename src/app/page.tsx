42a2a5f8e8ddcc390db399ccafa99ff13f091672
yamada_co_jp <f.atsuya.personal@gmail.com>
2024-11-01 02:45:49 +0900

    five

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,15 +8,23 @@ const Home = () => {
         <Image
           src="/business-card/1.png"
           alt="Profile Picture"
-          width={150}
-          height={150}
-          className="rounded-full mb-4 md:mb-0 md:mr-8 shadow-neumorphism"
+          width={300}
+          height={300}
+          className="rounded-full mb-10 md:mb-0 md:mr-8 shadow-neumorphism"
           unoptimized
         />
         <div>
-          <h1 className="text-3xl font-semibold text-gray-800">古田　篤哉</h1>
-          <p className="text-gray-500">27卒IT学生</p>
-          <p className="mt-2 text-gray-600">f.atsuya.personal@gmail.com</p>
+          <h1 className="text-3xl font-semibold text-gray-800 mb-6">古田　篤哉</h1>
+          <p className="text-gray-500 mb-3">27卒IT学生</p>
+          <p className="text-gray-600 mb-5">f.atsuya.personal@gmail.com</p>
+          <p className="text-gray-600">趣味</p>
+          <p className="text-gray-500 mb-6">ライブ、旅行、カラオケ、ギター</p>
+          <p className="text-gray-600">推し</p>
+          <p className="text-gray-500 mb-6">Ado</p>
+          <p className="text-gray-600">好きな言葉</p>
+          <p className="text-gray-500 mb-6">Life does not have to be perfect to be wonderful</p>
+          <p className="text-gray-600">スキル</p>
+          <p className="text-gray-500 mb-6">HTML/CSS/JavaScript/TypeScript/React/Next.js</p>
           <div className="flex justify-center md:justify-start mt-4 space-x-4">
             <a href="https://x.com/yamadacojp27" target="_blank" className="text-blue-500 hover:text-blue-600">Twitter</a>
             <a href="https://github.com/yamadacojp27" target="_blank" className="text-blue-500 hover:text-blue-600">GitHub</a>
