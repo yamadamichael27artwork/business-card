b80ebe8631332b5a38f2d677ac87819479252409
yamada_co_jp <f.atsuya.personal@gmail.com>
2024-11-01 00:58:01 +0900

    five

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   basePath: '/business-card',
+  assetPrefix: '/business-card',
 };
 
 export default nextConfig;
